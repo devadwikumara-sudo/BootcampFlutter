a45025772bed965b99c6c54ad0c503f5fe6cd1cf
devadwikumara-sudo <devakumara1927@gmail.com>
2026-09-02 00:28:49 +0700

    Acara 6 implementasi conditional Dart

---
+++ Acara-6/conditional.dart
@@ -0,0 +1,20 @@
+void main() {
+  int pilihan = 2;
+
+  switch (pilihan) {
+    case 1:
+      print("Nasi Goreng");
+      break;
+
+    case 2:
+      print("Mie Ayam");
+      break;
+
+    case 3:
+      print("Bakso");
+      break;
+
+    default:
+      print("Pilihan tidak tersedia");
+  }
+}
